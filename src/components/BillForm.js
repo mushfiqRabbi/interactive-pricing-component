d71b65cbc99f57ff024c2f50eb8d0a1fc221675c
mushfiq <mushfiqxrabbi@gmail.com>
2023-03-30 12:12:33 +0600

    get data from server

--- src/components/BillForm.js
+++ src/components/BillForm.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Amount from "./Amount";
-import Footer from "./Footer";
 import Slider from "./Slider";
 import Switch from "./Switch";
 import Vcounts from "./Vcounts";
@@ -8,12 +7,32 @@ import Vcounts from "./Vcounts";
 export default function BillForm() {
   const [sliderVal, setSliderVal] = useState(3);
   const [isYearly, setIsYearly] = useState(false);
+  const [views, setViews] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(
+        `http://localhost:5000/ipc/${Number(sliderVal)}?yearly=${isYearly}`
+      );
+      const data = await res.json();
+      setViews(data.views);
+      setAmount(data.amount);
+    };
+    getData();
+  }, [sliderVal, isYearly]);
 
   return (
     <div className="flex flex-col items-center w-full px-4 py-8 space-y-6">
-      <Vcounts sliderVal={sliderVal} />
-      <Slider sliderVal={sliderVal} setSliderVal={setSliderVal} />
-      <Amount sliderVal={sliderVal} isYearly={isYearly} />
+      <Vcounts views={views} />
+      <Slider
+        sliderVal={sliderVal}
+        setSliderVal={setSliderVal}
+        isYearly={isYearly}
+        setViews={setViews}
+        setAmount={setAmount}
+      />
+      <Amount amount={amount} />
       <Switch setIsYearly={setIsYearly} />
     </div>
   );
--- src/components/Vcounts.js
+++ src/components/Vcounts.js
@@ -1,25 +1,6 @@
 import React from "react";
 
-export default function Vcounts({ sliderVal }) {
-  let views;
-  switch (sliderVal) {
-    case 1:
-      views = "10k";
-      break;
-    case 2:
-      views = "50k";
-      break;
-    case 3:
-      views = "100k";
-      break;
-    case 4:
-      views = "500k";
-      break;
-    case 5:
-      views = "1M";
-      break;
-  }
-
+export default function Vcounts({ views }) {
   return (
     <div className="uppercase font-bold text-sm tracking-widest text-slate-500">
       {views} pageviews
